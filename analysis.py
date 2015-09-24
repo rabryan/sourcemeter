2bf5969b1bec7e1a7d90d308b2b4170a9000f3b8
Richard Bryan <rabryan@lucidsci.com>
2015-09-24 12:35:33 -0400

    drift project script support

--- analysis.py
+++ analysis.py
@@ -1,18 +1,88 @@
 import matplotlib.pyplot as plot
 import sys
 import csv
-
+import math
+import scipy
+import scipy.optimize
+import scipy.stats as stats
+import numpy
 
 if __name__ == "__main__":
 
     f = open(sys.argv[1])
     reader = csv.reader(f, delimiter='\t')
     data = list(reader)
-    vs = [float(v[0:-1]) for i,t,v in data]
-    ts = [float(t) for i,t,v in data]
+    for d in data:
+        if len(d) != 3:
+            print d
+            data.remove(d)
+
+    vs = []
+    ts = []
+    for i,t,v in data:
+        try:
+            v_f = float(v[0:-1])
+            t_f = float(t)
+            vs.append(v_f)
+            ts.append(t_f)
+        except:
+            print("ignoring bad values: {}  {}  {}".format(i, t, v))
+            pass
+
     tstart = ts[0]
     trels = [t - tstart for t in ts]
     thrs = [t/3600.0 for t in trels]
 
+    n = 0.115
+    tau = 1.13
+    decay1 = [(3.05-n) + n*math.exp(-t/tau) for t in thrs]
+    delta = [abs(v1-v2) for v1,v2 in zip(vs[:-1], vs[1:])]
+    print len(delta)
+    print len(thrs)
     plot.plot(thrs, vs)
+
+    def exp_decay(t, C, A, tau):
+        return C - A*numpy.exp(-t/tau)
+
+    mean = numpy.average(vs[:5])
+    vtops = []
+    ttops = []
+    for v, t in zip(vs, thrs):
+        if v > 0.99*mean:
+            vtops.append(v)
+            ttops.append(t)
+            mean = mean*(0.995) + v*0.005
+
+    """
+    vttops = []
+    tttops = []
+    mean = numpy.average()
+    for v, t in zip(vtops, ttops):
+        if v > mean:
+            vttops.append(v)
+            tttops.append(t)
+    """
+    N=40
+    vmaxes = [max(vs[i:i+N]) for i in range((len(vs)) - N)]
+    tmaxes = [thrs[i] for i in range((len(vs)) - N)]
+    vs = numpy.array(vs)
+    vtops = numpy.array(vtops)
+    ttops = numpy.array(ttops)
+    thrs = numpy.array(thrs)
+    vmaxes = numpy.array(vmaxes)
+    tmaxes = numpy.array(tmaxes)
+
+    x0 = numpy.array([3.05, 0.115, 1.113])
+    (C, A, tau), cov = scipy.optimize.curve_fit(exp_decay, tmaxes, vmaxes)
+
+    print("best fit: C={}  A={}  tau={}".format(C, A, tau))
+    print("{} - {}A*e^(-t/{})".format(C, A, tau))
+
+    v_best_fit = exp_decay(tmaxes, C, A, tau)
+    plot.plot(tmaxes, vmaxes, 'r')
+    plot.plot(tmaxes, v_best_fit, 'b--')
+    plot.show()
+
+    #voltage drop when active
+    plot.plot(thrs[:-1], delta, 'r')
     plot.show()
--- keithley.py
+++ keithley.py
@@ -6,7 +6,10 @@ a keithley 2400 sourcemeter via rs-232
 import serial
 import re
 import time
-SAMPLE_INT = 1
+import logging as log
+
+from drift.model.core import Source, Attr, register_source_class
+from drift.model.projects.project import ProjectScript, register_project_script
 
 def parse_reading(readstr):
     """
@@ -23,25 +26,79 @@ def parse_reading(readstr):
     return float(readstr[2:8])
 
 
+class Keithley( Source ):
+    sampleint = Attr("Sample Interval (s)", float, default=1, min=0.2, max=100000)
+    port = Attr("Serial Port", str, readonly=True, default="/dev/ttyUSB0")
+    
+    def __init__(self, **kwargs):
+        super().__init__(name="Sourcemeter", **kwargs)
+        self.com = serial.Serial(self.port, 9600, timeout=1)
+        self.newDatum("Voltage", units="v")
+        self._last_read = 0
+    
+    def start(self):
+        """ reset and set to measure voltage """
+        self.com.write("*rst\n".encode())
+        self.com.write("source:func curr\n".encode())
+        self.com.write("sense:func 'voltage'\n'".encode())
+        self.com.write("trig:count 1\n".encode())
+        self.com.write("form:elem volt\n".encode())
+        self.com.write("outp on\n".encode())
+        self.read()
+
+    def tic(self):
+        if time.time() - self._last_read >= self.sampleint:
+            self.read()
+            self._last_read = time.time()
+
+    def read(self):
+        self.com.write("read?\n".encode())
+
+        output = self.com.readall()
+        value = parse_reading(output.decode())
+        if value:
+            self.broadcast( "Voltage", time.time(), value )
+            log.debug("Voltage: {}".format(value))
+        else:
+            log.warning("Invalid sourcemeter response \'{}\'".format( output.decode()))
+
+
+
+@register_project_script("Keithley")
+class KeithleyScript(ProjectScript):
+    def __init__( self, project):
+        super().__init__()
+        self.sourcemeter = Keithley() 
+        self.connectJobTimer(project)
+        project.add(self.sourcemeter)
+
+    def start(self):
+        self.sourcemeter.start()
+    
+    def stop(self):
+        self.sourcemeter.stop()
+        self.removeJobs()
+
 if __name__ == "__main__":
     print(time.ctime())
+    SAMPLE_INT = 1
     tstart = time.time()
     com = serial.Serial("/dev/ttyUSB0", 9600, timeout=1)
 
     """ reset and set to measure voltage """
-    com.write("*rst\n")
-    com.write("source:func curr\n")
-    com.write("sense:func 'voltage'\n'")
-    com.write("trig:count 1\n")
-    com.write("form:elem volt\n")
-    com.write("outp on\n")
+    com.write("*rst\n".encode())
+    com.write("source:func curr\n".encode())
+    com.write("sense:func 'voltage'\n'".encode())
+    com.write("trig:count 1\n".encode())
+    com.write("form:elem volt\n".encode())
+    com.write("outp on\n".encode())
     i = 0
     while True:
         rstart = time.time()
-        com.write("read?\n")
+        com.write("read?\n".encode())
 
         output = com.readall()
-        value = parse_reading(output)
+        value = parse_reading(output.decode())
         if value:
             print("{}\t{}\t{}v".format(i, rstart, value))
         else:
